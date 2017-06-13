96b7ffa6ebc16c896dee126a6b79176303ba2a14
Pavel Apalkov <apalkov.pasha@yandex.ru>
2017-06-13 21:54:58 +0400

    Create config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-token = '350216540:AAF_QcZyhUn-wRC-Ro2wVlV_dcHymFVupRo'
-dict_key = 'trnsl.1.1.20170602T175835Z.19d971202e528c4f.25d79d7df6bbba4b2177588a6eaf0e5f59c2c1e8'
+token = 'TOKEN'
+dict_key = 'KEY'
 
